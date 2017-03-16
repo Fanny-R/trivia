dbf256c0afb95fa8423abf5b9f8ebcbfbea4aa69
fanny.rottee@gmail.com <martin.bolot@etu.univ-lyon1.fr>
2017-03-16 15:41:54 +0100

    Add class Player

---
+++ C#/Trivia/Trivia/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    class Player
+    {
+        private string _name;
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}
