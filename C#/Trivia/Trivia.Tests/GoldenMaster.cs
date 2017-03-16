5f786eb6eeebb5107cc3fb631017c3dced24175e
fanny.rottee@gmail.com <martin.bolot@etu.univ-lyon1.fr>
2017-03-16 17:45:21 +0100

    Golden master with Approval Tests

---
+++ C#/Trivia/Trivia.Tests/GoldenMaster.cs
@@ -0,0 +1,24 @@
+﻿using ApprovalTests;
+using NUnit.Framework;
+using System;
+using System.IO;
+using ApprovalTests.Reporters;
+
+namespace Trivia.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class GoldenMaster
+    {
+        [Test]
+        public void ShouldNotChange()
+        {
+            var stringWriter = new StringWriter();
+            var previousConsoleWriter = Console.Out;
+            Console.SetOut(stringWriter);
+            GameRunner.Main(null);
+            Console.SetOut(previousConsoleWriter);
+            Approvals.Verify(stringWriter.ToString());
+        }
+
+    }
+}
