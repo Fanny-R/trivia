b7c23df09ce1227039ab955dc4b3877cbfd29072
Fanny-R <fanny.rottee@gmail.com>
2017-03-30 17:30:24 +0200

    Create class Players

---
+++ C#/Trivia/Trivia/Players.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia
+{
+    class Players
+    {
+        public List<Player> ListPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
+
+        public void AddPlayer(Player player)
+        {
+            ListPlayer.Add(player);
+        }
+
+        public int NumberOfPlayers()
+        {
+            return ListPlayer.Count();
+        }
+    }
+}
