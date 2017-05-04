8adc053dd429e142ea9c420b776172956592621c
Fanny-R <fanny.rottee@gmail.com>
2017-05-04 17:55:11 +0200

    Add tests for QuestionsFromFileRepository

---
+++ C#/Trivia/Trivia.Tests/QuestionsFromFileRepositoryShould.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Trivia.Tests
+{
+    public class QuestionsFromFileRepositoryShould
+    {
+        [Test]
+        public void GetQuestionsInFileAccordingToCategoryName()
+        {
+            IQuestionsRepository generatesQuestionsRepository = new QuestionsFromFileRepository();
+
+            Assert.That(generatesQuestionsRepository.GetQuestions("Pop").Contains("Question Pop 1"), Is.True);
+        }
+    }
+}
