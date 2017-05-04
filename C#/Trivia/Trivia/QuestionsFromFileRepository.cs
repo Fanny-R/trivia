e78bdf6f8ed8a37fff52ff5346cbf008452a5425
Fanny-R <fanny.rottee@gmail.com>
2017-05-04 17:53:07 +0200

    Add a repository generating questions from a file

---
+++ C#/Trivia/Trivia/QuestionsFromFileRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Trivia
+{
+    public class QuestionsFromFileRepository : IQuestionsRepository
+    {
+        public LinkedList<string> GetQuestions(string category)
+        {
+            return new LinkedList<string>(System.IO.File.ReadAllLines(@"F:\dotNet\trivia\C#\Trivia\Trivia.Tests\" + category + ".txt"));
+        }
+    } 
+}
