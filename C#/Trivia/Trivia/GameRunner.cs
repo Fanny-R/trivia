45f628f7f93a5fae1394da4f34668ba2631ce9d9
Fanny-R <fanny.rottee@gmail.com>
2017-06-22 09:21:51 +0200

    Get questions from files

--- C#/Trivia/Trivia/GameRunner.cs
+++ C#/Trivia/Trivia/GameRunner.cs
@@ -15,7 +15,7 @@ namespace Trivia
                 players.AddPlayer("Pat");
                 players.AddPlayer("Sue");
 
-                var questions = new Questions(new GeneratedQuestionsRepository());
+                var questions = new Questions(new QuestionsFromFileRepository());
                 questions.AddQuestionStack("Pop");
                 questions.AddQuestionStack("Science");
                 questions.AddQuestionStack("Sports");
